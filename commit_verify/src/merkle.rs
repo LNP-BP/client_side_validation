17c929be8c714105ba573e6f59420d0eb7747008
Dr Maxim Orlovsky <orlovsky@pandoracore.com>
2021-05-13 19:03:26 +0200

    Completing LNPBP-81 implementation
    - MerkleNode uses single sha256 hash instead of double (to match BIP-340 tagging style)
    - MerkleNode is displayed in hexadecimal format without reversing byte order
    - The root of Merkle tree is hash-tagged with root commitment tag

--- commit_verify/src/merkle.rs
+++ commit_verify/src/merkle.rs
@@ -20,7 +20,7 @@
 use std::io;
 use std::iter::FromIterator;
 
-use bitcoin_hashes::{sha256, sha256d, Hash, HashEngine};
+use bitcoin_hashes::{sha256, Hash, HashEngine};
 
 use crate::{commit_encode, CommitEncode, CommitVerify, ConsensusCommit};
 
@@ -30,16 +30,17 @@ use crate::{commit_encode, CommitEncode, CommitVerify, ConsensusCommit};
 pub trait ConsensusMerkleCommit:
     ConsensusCommit<Commitment = MerkleNode>
 {
-    /// The tag which will be used in the merklization process (see
+    /// The tag prefix which will be used in the merklization process (see
     /// [`merklize`])
-    const MERKLE_NODE_TAG: &'static str;
+    const MERKLE_NODE_PREFIX: &'static str;
 }
 
 hash_newtype!(
     MerkleNode,
-    sha256d::Hash,
+    sha256::Hash,
     32,
-    doc = "A hash of a arbitrary Merkle tree branch or root"
+    doc = "A hash type for LNPBP-81 Merkle tree leaves, branches and root",
+    false // We do not reverse displaying MerkleNodes in hexadecimal
 );
 
 impl strict_encoding::Strategy for MerkleNode {
@@ -88,7 +89,23 @@ where
     let mut tag_engine = sha256::Hash::engine();
     tag_engine.input(prefix.as_bytes());
     tag_engine.input(":merkle:".as_bytes());
-    merklize_inner(&tag_engine, data, 0, false, None)
+
+    let width = data.len();
+
+    // Tagging merkle tree root
+    let (root, height) = merklize_inner(&tag_engine, data, 0, false, None);
+    tag_engine.input("root:height=".as_bytes());
+    tag_engine.input(&height.to_string().into_bytes());
+    tag_engine.input(":width=".as_bytes());
+    tag_engine.input(&width.to_string().into_bytes());
+    let tag_hash = sha256::Hash::hash(&sha256::Hash::from_engine(tag_engine));
+    let mut engine = MerkleNode::engine();
+    engine.input(&tag_hash[..]);
+    engine.input(&tag_hash[..]);
+    root.commit_encode(&mut engine);
+    let tagged_root = MerkleNode::from_engine(engine);
+
+    (tagged_root, height)
 }
 
 fn merklize_inner<N>(
@@ -181,7 +198,9 @@ where
     }
 }
 
-/// The source data for the merklization process
+/// The source data for the [LNPBP-81] merklization process.
+///
+/// [LNPBP-81]: https://github.com/LNP-BP/LNPBPs/blob/master/lnpbp-0081.md
 #[derive(Clone, PartialEq, Eq, PartialOrd, Ord, Hash, Debug, Default)]
 pub struct MerkleSource<T>(
     /// Array of the data which will be merklized
@@ -217,7 +236,7 @@ where
             .iter()
             .map(L::consensus_commit)
             .collect::<Vec<MerkleNode>>();
-        merklize(L::MERKLE_NODE_TAG, &leafs).0.commit_encode(e)
+        merklize(L::MERKLE_NODE_PREFIX, &leafs).0.commit_encode(e)
     }
 }
 
@@ -240,11 +259,174 @@ where
 }
 
 /// Converts given piece of client-side-validated data into a structure which
-/// can be used in merklization process
+/// can be used in merklization process.
+///
+/// This dedicated structure is required since with
+/// `impl From<_> for MerkleSource` we would not be able to specify a concrete
+/// tagged hash, which we require in [LNPBP-81] merklization and which we
+/// provide here via [`ToMerkleSource::Leaf`]` associated type holding
+/// [`ConsensusMerkleCommit::MERKLE_NODE_PREFIX`] prefix value.
+///
+/// [LNPBP-81]: https://github.com/LNP-BP/LNPBPs/blob/master/lnpbp-0081.md
 pub trait ToMerkleSource {
     /// Defining type of the commitment produced during merlization process
     type Leaf: ConsensusMerkleCommit;
 
     /// Performs transformation of the data type into a merkilzable data
     fn to_merkle_source(&self) -> MerkleSource<Self::Leaf>;
+}
+
+#[cfg(test)]
+mod test {
+    use super::*;
+    use crate::commit_encode::{strategies, Strategy};
+    use crate::CommitConceal;
+    use amplify::{bmap, s};
+    use bitcoin_hashes::hex::ToHex;
+    use bitcoin_hashes::{sha256d, Hash};
+    use std::collections::BTreeMap;
+    use strict_encoding::StrictEncode;
+
+    #[test]
+    fn collections() {
+        // First, we define a data type
+        #[derive(
+            Clone,
+            PartialEq,
+            Eq,
+            PartialOrd,
+            Ord,
+            Hash,
+            Debug,
+            StrictEncode,
+            StrictDecode,
+        )]
+        struct Item(pub String);
+        // Next, we say that it should be concealed using some function
+        // (double SHA256 hash in this case)
+        impl CommitConceal for Item {
+            type ConcealedCommitment = sha256d::Hash;
+            fn commit_conceal(&self) -> Self::ConcealedCommitment {
+                sha256d::Hash::hash(self.0.as_bytes())
+            }
+        }
+        // Next, we need to specify how the concealed data should be
+        // commit-encoded: this time we strict-serialize the hash
+        impl Strategy for sha256d::Hash {
+            type Strategy = strategies::UsingStrict;
+        }
+        // Now, we define commitment encoding for our concealable type: it
+        // should conceal the data
+        impl Strategy for Item {
+            type Strategy = strategies::UsingConceal;
+        }
+        // Now, we need to say that consensus commit procedure should produce
+        // a final commitment from commit-encoded data (equal to the
+        // strict encoding of the conceal result) using `CommitVerify` type.
+        // Here, we use another round of hashing, producing merkle node hash
+        // from the concealed data.
+        impl ConsensusCommit for Item {
+            type Commitment = MerkleNode;
+        }
+        // Next, we need to provide merkle node tags for each type of the tree
+        impl ConsensusMerkleCommit for Item {
+            const MERKLE_NODE_PREFIX: &'static str = "item";
+        }
+        impl ConsensusMerkleCommit for (usize, Item) {
+            const MERKLE_NODE_PREFIX: &'static str = "usize->item";
+        }
+
+        impl ToMerkleSource for BTreeMap<usize, Item> {
+            type Leaf = (usize, Item);
+            fn to_merkle_source(&self) -> MerkleSource<Self::Leaf> {
+                self.iter().map(|(k, v)| (*k, v.clone())).collect()
+            }
+        }
+
+        let item = Item(s!("Some text"));
+        assert_eq!(&b"\x09\x00Some text"[..], item.strict_serialize().unwrap());
+        assert_eq!(
+            "6680bbec0d05d3eaac9c8b658c40f28d2f0cb0f245c7b1cabf5a61c35bd03d8e",
+            item.commit_serialize().to_hex()
+        );
+        assert_eq!(
+            "3e4b2dcf9bca33400028c8947565c1ff421f6d561e9ec48f88f0c9a24ebc8c30",
+            item.consensus_commit().to_hex()
+        );
+        assert_ne!(item.commit_serialize(), item.strict_serialize().unwrap());
+        assert_eq!(
+            MerkleNode::hash(&item.commit_serialize()),
+            item.consensus_commit()
+        );
+
+        let original = bmap! {
+            0usize => Item(s!("My first case")),
+            1usize => Item(s!("My second case with a very long string")),
+            3usize => Item(s!("My third case to make the Merkle tree two layered"))
+        };
+        let collection = original.to_merkle_source();
+        assert_eq!(
+            &b"\x03\x00\
+             \x00\x00\
+             \x0d\x00\
+             My first case\
+             \x01\x00\
+             \x26\x00\
+             My second case with a very long string\
+             \x03\x00\
+             \x31\x00\
+             My third case to make the Merkle tree two layered"[..],
+            original.strict_serialize().unwrap()
+        );
+        assert_eq!(
+            "3b970fa581bcdf4987e6455e2e5a1ea575bcb3f5b37c25600f600cc8d44e5598",
+            collection.commit_serialize().to_hex()
+        );
+        assert_eq!(
+            "3b970fa581bcdf4987e6455e2e5a1ea575bcb3f5b37c25600f600cc8d44e5598",
+            collection.consensus_commit().to_hex()
+        );
+        assert_ne!(
+            collection.commit_serialize(),
+            original.strict_serialize().unwrap()
+        );
+        assert_eq!(
+            MerkleNode::from_slice(&collection.commit_serialize()).unwrap(),
+            collection.consensus_commit()
+        );
+
+        let original = vec![
+            Item(s!("My first case")),
+            Item(s!("My second case with a very long string")),
+            Item(s!("My third case to make the Merkle tree two layered")),
+        ];
+        let vec: MerkleSource<Item> = original.clone().into();
+        assert_eq!(
+            &b"\x03\x00\
+             \x0d\x00\
+             My first case\
+             \x26\x00\
+             My second case with a very long string\
+             \x31\x00\
+             My third case to make the Merkle tree two layered"[..],
+            original.strict_serialize().unwrap()
+        );
+        assert_eq!(
+            "4ee97b1318fe417cac30790335033ece29ea4b6183ebf51d083d5a2e89ac33da",
+            vec.commit_serialize().to_hex()
+        );
+        assert_eq!(
+            "4ee97b1318fe417cac30790335033ece29ea4b6183ebf51d083d5a2e89ac33da",
+            vec.consensus_commit().to_hex()
+        );
+        assert_ne!(
+            vec.commit_serialize(),
+            original.strict_serialize().unwrap()
+        );
+        assert_eq!(
+            MerkleNode::from_slice(&vec.commit_serialize()).unwrap(),
+            vec.consensus_commit()
+        );
+        assert_ne!(vec.consensus_commit(), collection.consensus_commit());
+    }
 }
