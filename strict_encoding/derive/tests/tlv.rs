f4d61f8b49524aab64cb11a3dcc87f1b10769139
Dr Maxim Orlovsky <orlovsky@pandoracore.com>
2021-08-31 11:19:25 +0200

    Fixing encoding derive helpers bug with TLV non-copy encoding

--- strict_encoding/derive/tests/tlv.rs
+++ strict_encoding/derive/tests/tlv.rs
@@ -238,7 +238,7 @@ fn tlv_unknown() -> Result {
         tlv_int: Option<u16>,
 
         #[network_encoding(tlv = 2)]
-        tlv_int2: Option<u16>,
+        tlv_int2: Option<String>,
 
         #[network_encoding(unknown_tlvs)]
         rest_of_tlvs: BTreeMap<usize, Box<[u8]>>,
