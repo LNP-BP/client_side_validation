833007ffd89731c5bc8c4c3057ad9be271bdcfb4
Dr Maxim Orlovsky <orlovsky@pandoracore.com>
2022-04-05 18:56:20 +0200

    Remove strict encoding impl for PSBT. Closes #67

--- strict_encoding/src/bitcoin.rs
+++ strict_encoding/src/bitcoin.rs
@@ -18,7 +18,6 @@ use std::io::{Read, Write};
 use bitcoin::secp256k1::{ecdsa, schnorr, Secp256k1};
 use bitcoin::util::address::{self, Address, WitnessVersion};
 use bitcoin::util::bip32;
-use bitcoin::util::psbt::PartiallySignedTransaction;
 use bitcoin::util::taproot::{
     ControlBlock, FutureLeafVersion, LeafVersion, TapBranchHash, TapLeafHash,
     TapSighashHash, TapTweakHash, TaprootMerkleBranch,
@@ -424,9 +423,6 @@ impl Strategy for TxIn {
     type Strategy = strategies::BitcoinConsensus;
 }
 impl Strategy for Transaction {
-    type Strategy = strategies::BitcoinConsensus;
-}
-impl Strategy for PartiallySignedTransaction {
     type Strategy = strategies::BitcoinConsensus;
 }
 
@@ -1219,34 +1215,6 @@ pub(crate) mod test {
 
         test_encoding_roundtrip(&txout_segwit, &txout_segwit_bytes).unwrap();
         test_encoding_roundtrip(&txout_legacy, &txout_legacy_bytes).unwrap();
-    }
-
-    #[test]
-    fn test_psbt() {
-        let psbt_bytes = Vec::from_hex(
-            "70736274ff0100750200000001268171371edff285e937adeea4b37b78000c0566\
-            cbb3ad64641713ca42171bf60000000000feffffff02d3dff505000000001976a91\
-            4d0c59903c5bac2868760e90fd521a4665aa7652088ac00e1f5050000000017a914\
-            3545e6e33b832c47050f24d3eeb93c9c03948bc787b32e1300000100fda50101000\
-            00000010289a3c71eab4d20e0371bbba4cc698fa295c9463afa2e397f8533ccb62f\
-            9567e50100000017160014be18d152a9b012039daf3da7de4f53349eecb985fffff\
-            fff86f8aa43a71dff1448893a530a7237ef6b4608bbb2dd2d0171e63aec6a4890b4\
-            0100000017160014fe3e9ef1a745e974d902c4355943abcb34bd5353ffffffff020\
-            0c2eb0b000000001976a91485cff1097fd9e008bb34af709c62197b38978a4888ac\
-            72fef84e2c00000017a914339725ba21efd62ac753a9bcd067d6c7a6a39d0587024\
-            7304402202712be22e0270f394f568311dc7ca9a68970b8025fdd3b240229f07f8a\
-            5f3a240220018b38d7dcd314e734c9276bd6fb40f673325bc4baa144c800d2f2f02\
-            db2765c012103d2e15674941bad4a996372cb87e1856d3652606d98562fe39c5e9e\
-            7e413f210502483045022100d12b852d85dcd961d2f5f4ab660654df6eedcc794c0\
-            c33ce5cc309ffb5fce58d022067338a8e0e1725c197fb1a88af59f51e44e4255b20\
-            167c8684031c05d1f2592a01210223b72beef0965d10be0778efecd61fcac6f79a4\
-            ea169393380734464f84f2ab300000000000000"
-        ).unwrap();
-
-        let psbt: PartiallySignedTransaction =
-            consensus::deserialize(&psbt_bytes).unwrap();
-
-        test_encoding_roundtrip(&psbt, &psbt_bytes).unwrap();
     }
 
     #[test]
