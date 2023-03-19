8ae575d39c647922bb566450e5c4fbff541b9fe0
Dr Maxim Orlovsky <orlovsky@lnp-bp.org>
2023-03-19 15:28:44 +0100

    stl: update scripts

--- scripts/typelib.sh
+++ scripts/typelib.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 
-cargo run --features stl --package commit_verify commit-stl -s
-cargo run --features stl --package commit_verify commit-stl -b
-cargo run --features stl --package commit_verify commit-stl -h
+cargo run --features stl --package commit_verify --bin commit-stl -- --stl
+cargo run --features stl --package commit_verify --bin commit-stl -- --sty
+cargo run --features stl --package commit_verify --bin commit-stl -- --asc
