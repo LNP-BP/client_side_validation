b7b6aafd5abd3765a8009857ce9369e108562217
Dr Maxim Orlovsky <orlovsky@lnp-bp.org>
2024-02-28 11:15:00 +0100

    stl: generate vesper file for merklization workflows

--- commit_verify/src/bin/commit-stl.rs
+++ commit_verify/src/bin/commit-stl.rs
@@ -12,15 +12,20 @@
 // You should have received a copy of the Apache 2.0 License along with this
 // software. If not, see <https://opensource.org/licenses/Apache-2.0>.
 
-use commit_verify::stl;
-use strict_types::parse_args;
+use std::fs;
+use std::io::Write;
+
+use commit_verify::stl::commit_verify_stl;
+use commit_verify::{mpc, CommitmentLayout, MerkleNode};
+use strict_types::stl::{std_stl, strict_types_stl};
+use strict_types::{parse_args, SystemBuilder};
 
 fn main() {
-    let lib = stl::commit_verify_stl();
+    let lib = commit_verify_stl();
     let (format, dir) = parse_args();
     lib.serialize(
         format,
-        dir,
+        dir.as_ref(),
         "0.1.0",
         Some(
             "
@@ -31,4 +36,56 @@ fn main() {
         ),
     )
     .expect("unable to write to the file");
+
+    let dir = dir.unwrap_or_else(|| ".".to_owned());
+
+    let std = std_stl();
+    let st = strict_types_stl();
+    let cv = commit_verify_stl();
+
+    let sys = SystemBuilder::new()
+        .import(cv)
+        .unwrap()
+        .import(st)
+        .unwrap()
+        .import(std)
+        .unwrap()
+        .finalize()
+        .expect("not all libraries present");
+
+    let mut file = fs::File::create(format!("{dir}/Merkle.vesper")).unwrap();
+    writeln!(
+        file,
+        "{{-
+  Description: Merklization and MPC workflows
+  Author: Dr Maxim Orlovsky <orlovsky@lnp-bp.org>
+  Copyright (C) 2024 LNP/BP Standards Association. All rights reserved.
+  License: Apache-2.0
+-}}
+
+Merklization vesper lexicon=types+commitments
+"
+    )
+    .unwrap();
+    writeln!(file, "\n-- General merklization workflows\n").unwrap();
+    let layout = MerkleNode::commitment_layout();
+    writeln!(file, "{layout}").unwrap();
+    let tt = sys.type_tree("CommitVerify.MerkleNode").unwrap();
+    writeln!(file, "{tt}").unwrap();
+
+    writeln!(file, "\n-- Multi-protocol commitment workflows\n").unwrap();
+    let layout = mpc::Leaf::commitment_layout();
+    writeln!(file, "{layout}").unwrap();
+    let tt = sys.type_tree("CommitVerify.Leaf").unwrap();
+    writeln!(file, "{tt}").unwrap();
+
+    let layout = mpc::MerkleBlock::commitment_layout();
+    writeln!(file, "{layout}").unwrap();
+    let tt = sys.type_tree("CommitVerify.MerkleBlock").unwrap();
+    writeln!(file, "{tt}").unwrap();
+
+    let layout = mpc::MerkleTree::commitment_layout();
+    writeln!(file, "{layout}").unwrap();
+    let tt = sys.type_tree("CommitVerify.MerkleTree").unwrap();
+    writeln!(file, "{tt}").unwrap();
 }
